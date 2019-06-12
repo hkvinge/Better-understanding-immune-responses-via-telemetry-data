ba24f7a3e2867af2a2d95480dc7896d303efddaa
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2019-06-12 11:08:52 -0600

    started figures folder

---
+++ figures/README.txt
@@ -0,0 +1,2 @@
+put all data necessary to generate figures in this folder.
+each figure should go in its own subfolder to avoid clutter in the main figures/ directory.
---
+++ figures/anomaly_detection_comparison/online_mset_analysis.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('/home/katrina/a/aminian/Better-understanding-immune-responses-via-telemetry-data')
+sys.path.append('/home/katrina/a/aminian/Better-understanding-immune-responses-via-telemetry-data/online_mset')
+
+import tde
+import mset
+import load_csvs
+
+tdict,df = load_csvs.load()
+
+mice = ['CC004-021', 'CBA-218']
+
+# wow, parameters
+thresh = 0.05
+delay = 6*60    # six hours
+
+figs = []
+axs = []
+
+
+for m in mice:
+    # cheat some - mean-center the data with
+    # some amount of pre-infection data.
+    temp = tdict[m]
+    temp -= sum(temp[:4*delay])/(4.*delay)
+    
+    fig,ax = mset.visualize_mset(temp,thresh,delay)
+    figs.append(fig)
+    axs.append(ax)
+    fig.show()
+#
